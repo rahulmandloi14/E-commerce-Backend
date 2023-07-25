a79bf779928d51ab38320fee8124083e548fd2e9
rahulmandloi14 <mandloirahul61@gmail.com>
2023-07-25 22:02:52 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ module.exports = {
    * This gives you an opportunity to extend code.
    */
   register(/*{ strapi }*/) {},
-
+  
   /**
    * An asynchronous bootstrap function that runs before
    * your application gets started.
